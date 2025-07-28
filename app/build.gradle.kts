8da377bac683de28628cf4ebbc4ccb2550ab84f6
misaka10032w <simple6566@gmail.com>
2025-07-28 22:16:38 +0800

    升级依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     alias(libs.plugins.com.google.firebase.crashlytics)
     alias(libs.plugins.com.google.firebase.firebase.pref)
     alias(libs.plugins.compose.compiler)
-    id("com.mikepenz.aboutlibraries.plugin") version "12.2.3"
+    id("com.mikepenz.aboutlibraries.plugin") version "12.2.4"
     id("com.github.ben-manes.versions") version "0.52.0"
     // alias(libs.plugins.compose.compiler)
 }
