d5c4e44673b165014c96b621b3cb5c5d15788818
misaka10032w <simple6566@gmail.com>
2025-08-02 19:06:58 +0800

    Announcement被混淆导致崩溃

--- app/src/main/java/com/yenaly/han1meviewer/logic/model/Announcement.kt
+++ app/src/main/java/com/yenaly/han1meviewer/logic/model/Announcement.kt
@@ -1,15 +1,16 @@
 package com.yenaly.han1meviewer.logic.model
 
+import androidx.annotation.Keep
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-
+@Keep
 data class Announcement(
-    val title: String = "",
-    val content: String = "",
-    val timestamp: Long = 0,
-    val priority: Int = 2,
-    val imageUrl: String = "",
+    @JvmField val title: String = "",
+    @JvmField val content: String = "",
+    @JvmField val timestamp: Long = 0,
+    @JvmField val priority: Int = 2,
+    @JvmField val imageUrl: String = "",
     @JvmField val isActive: Boolean = false
 ) {
     fun getFormattedDate(): String {
