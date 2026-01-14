9e1cdcec08c986df45dfd811a4d808694fa2e976
misaka10032w <simple6566@gmail.com>
2026-01-14 23:29:58 +0800

    去除似乎毫无屌用的二维码导入cookies功能，减小安装包体积

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -155,13 +155,6 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
 
-    //for QRCODE scanner
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.view)
-    implementation(libs.barcode.scanning)
-
     // datetime
 
     implementation(libs.datetime)
