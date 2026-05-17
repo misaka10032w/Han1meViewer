f3eb5eebf58069442b9e49fb3e5b41fe4dca5f59
misaka10032w <simple6566@gmail.com>
2026-05-17 13:37:52 +0800

    统一设置页标题栏风格

--- app/src/main/java/com/yenaly/han1meviewer/ui/navigation/settings/SettingsNavHost.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/navigation/settings/SettingsNavHost.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -56,7 +56,7 @@ fun SettingsScaffold(
                 TopAppBar(
                     title = { Text(stringResource(currentDestination.titleRes)) },
                     navigationIcon = {
-                        IconButton(onClick = ::navigateBack) {
+                        FilledIconButton(onClick = ::navigateBack) {
                             Icon(
                                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                                 contentDescription = stringResource(R.string.back),
