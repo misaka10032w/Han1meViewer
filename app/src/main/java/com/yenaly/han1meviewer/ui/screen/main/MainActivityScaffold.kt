f3fb30eae02f0636af705d7b2acdd807a16bb3c7
misaka10032w <simple6566@gmail.com>
2026-05-20 21:25:18 +0800

    drawer允许被沉浸到导航栏后方

--- app/src/main/java/com/yenaly/han1meviewer/ui/screen/main/MainActivityScaffold.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/screen/main/MainActivityScaffold.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -65,7 +64,6 @@ fun MainActivityScaffold(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet(
-                modifier = Modifier.navigationBarsPadding(),
                 drawerContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
             ) {
                 MainDrawerHeader(
