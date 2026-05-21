71d127a9f6d7c38f9ccaa037f334db36976fc718
misaka10032w <simple6566@gmail.com>
2026-05-21 17:03:08 +0800

    允许平板模式自定义部分界面列数

--- app/src/main/java/com/yenaly/han1meviewer/ui/screen/settings/dialog/LicenseDialog.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/screen/settings/dialog/LicenseDialog.kt
@@ -1,4 +1,4 @@
-package com.yenaly.han1meviewer.ui.screen.settings
+package com.yenaly.han1meviewer.ui.screen.settings.dialog
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
