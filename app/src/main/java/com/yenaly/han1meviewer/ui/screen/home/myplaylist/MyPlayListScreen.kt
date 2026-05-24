ee5a608d212800d2e050feaa602ee98c34dc7831
misaka10032w <simple6566@gmail.com>
2026-05-24 16:34:47 +0800

    优化Myplaylist源码结构

--- app/src/main/java/com/yenaly/han1meviewer/ui/screen/home/myplaylist/MyPlayListScreen.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/screen/home/myplaylist/MyPlayListScreen.kt
@@ -58,7 +58,6 @@ import com.yenaly.han1meviewer.logic.model.Playlists
 import com.yenaly.han1meviewer.logic.state.PageLoadingState
 import com.yenaly.han1meviewer.logic.state.WebsiteState
 import com.yenaly.han1meviewer.ui.component.LoadMoreFooter
-import com.yenaly.han1meviewer.ui.component.PlaylistItem
 import com.yenaly.han1meviewer.ui.component.PullRefreshOverlay
 import com.yenaly.han1meviewer.ui.component.appbar.HanimeScaffold
 import com.yenaly.han1meviewer.ui.component.content.EmptyContent
