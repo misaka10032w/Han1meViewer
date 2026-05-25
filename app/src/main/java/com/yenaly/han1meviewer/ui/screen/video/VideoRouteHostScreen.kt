2bbd50cd5fd8d02321c513b44850b169f71609f8
misaka10032w <simple6566@gmail.com>
2026-05-25 21:45:37 +0800

    平板模式下，推荐栏允许折叠

--- app/src/main/java/com/yenaly/han1meviewer/ui/screen/video/VideoRouteHostScreen.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/screen/video/VideoRouteHostScreen.kt
@@ -451,7 +451,6 @@ fun VideoRouteHostScreen(
     LaunchedEffect(
         hostUiState.isInPipMode,
         isSideRelatedCollapsed,
-        activity.resources.configuration.orientation,
     ) {
         if (hostUiState.isInPipMode) return@LaunchedEffect
         val height = if (Preferences.tabletMode) {
@@ -459,8 +458,10 @@ fun VideoRouteHostScreen(
         } else {
             250.dp
         }
-        viewModel.setPlayerHeightDp(height)
-        setPlayerHeight(height)
+        if (hostUiState.playerHeightDp != height) {
+            viewModel.setPlayerHeightDp(height)
+            setPlayerHeight(height)
+        }
     }
 
     LaunchedEffect(route.videoCode, route.localUri) {
