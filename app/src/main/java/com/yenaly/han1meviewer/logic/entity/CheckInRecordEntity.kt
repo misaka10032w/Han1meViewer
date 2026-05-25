c32a569226ff6ece5c71dea3ce51ffb681da55e7
misaka10032w <simple6566@gmail.com>
2026-05-26 00:44:04 +0800

    支持设置数据   自定义 H 关键帧   冲了吗 / 打卡记录   观看历史   下载数据库元数据：   - download_groups   - HanimeDownloadEntity   - DownloadCategoryEntity   - HanimeCategoryCrossRef   的备份恢复

--- app/src/main/java/com/yenaly/han1meviewer/logic/entity/CheckInRecordEntity.kt
+++ app/src/main/java/com/yenaly/han1meviewer/logic/entity/CheckInRecordEntity.kt
@@ -2,7 +2,9 @@ package com.yenaly.han1meviewer.logic.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "check_in_records")
 data class CheckInRecordEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
