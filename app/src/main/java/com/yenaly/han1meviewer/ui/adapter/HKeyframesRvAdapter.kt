6badb4eff388156ff573cc9f0eca08cdc75ae94c
misaka10032w <simple6566@gmail.com>
2026-08-08 21:05:44 +0800

    修复播放页面H关键帧无法编辑和删除的问题 #435

--- app/src/main/java/com/yenaly/han1meviewer/ui/adapter/HKeyframesRvAdapter.kt
+++ app/src/main/java/com/yenaly/han1meviewer/ui/adapter/HKeyframesRvAdapter.kt
@@ -13,6 +13,7 @@ import com.yenaly.han1meviewer.R
 import com.yenaly.han1meviewer.logic.entity.HKeyframeEntity
 import com.yenaly.han1meviewer.ui.activity.MainActivity
 import com.yenaly.han1meviewer.util.showAlertDialog
+import com.yenaly.yenaly_libs.utils.findActivityOrNull
 import com.yenaly.yenaly_libs.utils.showShortToast
 
 /**
@@ -98,16 +99,14 @@ class HKeyframeRvAdapter(
                         setPositiveButton(R.string.confirm) { _, _ ->
                             val prompt = etPrompt.text.toString()
                             val pos = etPosition.text.toString().toLong()
-                            when (context) {
-                                is MainActivity -> {
-                                    context.viewModel.modifyHKeyframe(
-                                        videoCode, item, HKeyframeEntity.Keyframe(
-                                            position = pos,
-                                            prompt = prompt
-                                        )
+                            context.findActivityOrNull<MainActivity>()?.let { activity ->
+                                activity.viewModel.modifyHKeyframe(
+                                    videoCode, item, HKeyframeEntity.Keyframe(
+                                        position = pos,
+                                        prompt = prompt
                                     )
-                                     showShortToast(R.string.modify_success)
-                                }
+                                )
+                                showShortToast(R.string.modify_success)
                             }
                         }
                         setNegativeButton(R.string.cancel, null)
@@ -122,11 +121,9 @@ class HKeyframeRvAdapter(
                         setTitle(R.string.sure_to_delete)
                         setMessage(JZUtils.stringForTime(item.position))
                         setPositiveButton(R.string.confirm) { _, _ ->
-                            when (context) {
-                                is MainActivity -> {
-                                    context.viewModel.removeHKeyframe(videoCode, item)
-                                     showShortToast(R.string.delete_success)
-                                }
+                            context.findActivityOrNull<MainActivity>()?.let { activity ->
+                                activity.viewModel.removeHKeyframe(videoCode, item)
+                                showShortToast(R.string.delete_success)
                             }
                         }
                         setNegativeButton(R.string.cancel, null)
